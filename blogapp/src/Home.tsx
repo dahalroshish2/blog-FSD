396e904398e82dae80902ada1bd800ccbf413537
Roshish Dahal <dahalroshish2@gmail.com>
2024-01-19 14:13:10 +0530

    data fetch

--- blogapp/src/Home.tsx
+++ blogapp/src/Home.tsx
@@ -1,25 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Home(){
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-      ])
-    function handleDelete(id: number){
-        console.log(id);
-    }
+//let title ="This is my title";
+const [title, setTitle] = useState("This is my title");
+function changeMyTitle(){
+  setTitle (" This is my new title");
+  console.log(title);
+}
+useEffect(() => {
+  // Fetch data from JSONPlaceholder API when the component mounts
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(json => {
+      // Update the title with the fetched data
+      setTitle(json.title);
+    });
+}, []); 
+
     return(
     <div className="blog-list">
       <h2>Blog</h2>
-      {blogs.map(blog => (
-        <div className="blog-preview" key={blog.id} >
-          <h2>{ blog.title }</h2>
-          <p>Written by { blog.author }</p>
-          <button onClick={() => handleDelete(blog.id)}>delete blog</button>
-        </div>
-      ))}
-    </div>     
-    )
-}
+      <h2>{title}</h2>
+      <button onClick={changeMyTitle}
+      >Cick me</button>
+    
+    </div>
+    );}
 export default Home;
